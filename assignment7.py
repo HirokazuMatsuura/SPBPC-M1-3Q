64941fe694730efdcde96f11074239826c05bedc
HirokazuMatsuura <1hiro1.ww@gmail.com>
2021-12-07 23:35:55 +0900

    fix: assignment7 update func

--- assignment7.py
+++ assignment7.py
@@ -9,15 +9,21 @@ class UpdateValue:
         self.x = x
         self.w = w
         self.alpha = alpha
-        self.epsilon = 0.1
+        self.epsilon = 0.01
 
     def __differential(self, i, j):
         dki_dw = 0
         for x1 in range(2):
             for x3 in range(2):
-                dki_dw += float((-1)) * self.__q_x1(x1) * self.__q_x3_d_x1(x3, x1) * (self.__p_x2_d_x1_x3(x1, x3) - self.__p_x2_x3_d_x1(x1))
-                
-        return (-1) * self.alpha * self.x[i] * self.x[j] * dki_dw
+                dki_dw += float((-1)) * self.__q_x1(x1) * self.__q_x3_d_x1(x3, x1) * (self.__p_x2_d_x1_x3(x1, x3, i, j) - self.__p_x2_x3_d_x1(x1, i, j))
+
+        import sys
+        sys.exit()
+        return (-1) * self.alpha * dki_dw
+
+    def __xi_xj(self, i, j, x1, x2, x3):
+        x = np.array([x1, x2, x3])
+        return x[i] * x[j]
 
     def __q_x1(self, x1):
         val = 0
@@ -37,25 +43,25 @@ class UpdateValue:
                 bottom += self.q[x1, x2_, x3_]
         return top / bottom
 
-    def __p_x2_d_x1_x3(self, x1, x3):
+    def __p_x2_d_x1_x3(self, x1, x3, i, j):
         val = 0
         top = 0
         bottom = 0
         for x2_ in range(2):
-            top = self.p[x1, x2_, x3]
+            top = self.__xi_xj(i, j, x1, x2_, x3) * self.p[x1, x2_, x3]
             bottom = 0
             for x2__ in range(2):
                 bottom += self.p[x1, x2__, x3]
             val += top / bottom
         return val
 
-    def __p_x2_x3_d_x1(self, x1):
+    def __p_x2_x3_d_x1(self, x1, i, j):
         val = 0
         top = 0
         bottom = 0
         for x2_ in range(2):
             for x3_ in range(2):
-                top = self.p[x1, x2_, x3_]
+                top = self.__xi_xj(i, j, x1, x2_, x3_) * self.p[x1, x2_, x3_]
                 bottom = 0
                 for x2__ in range(2):
                     for x3__ in range(2):
@@ -63,7 +69,7 @@ class UpdateValue:
                 val += top / bottom
         return val
 
-    def __update_stop(self, w_old, w_new, threshold=10**(-10)):
+    def __update_stop(self, w_old, w_new, threshold=10**(-6)):
         if threshold > np.abs(w_new - w_old):
             return True
         else:
@@ -89,10 +95,10 @@ if __name__ == "__main__":
     joint_q = np.random.rand(2, 2, 2)
     joint_q = joint_q / np.sum(joint_q)
 
-    w = np.zeros((3, 3))
+    w = np.random.rand(3, 3)
     x = np.ones((3))
 
-    alpha = 0.00001
+    alpha = 0.1
 
     update_w = UpdateValue(joint_p, joint_q, x, w, alpha)
 
